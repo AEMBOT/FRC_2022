633def8830be3cba01ee6138811a852d3e803fb9
Ethan <1bluejellybean@gmail.com>
2022-02-09 16:51:43 -0800

    organized variables and moved shooter physics into shooter subsystem class

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -121,6 +121,13 @@ public class ShooterSubsystem extends SubsystemBase {
   public int angle = 24;
   public double slope24 = 4000/24;
   public double slope4 = 1400/4;
+  private double maxHeight; 
+  private int shooterAngle = 24;
+  private double shooterHeight = 0.3048;
+  private final double gravity = 9.8;
+  private int flywheelRadius = 4;
+  private double targetHeight = 2.7432;
+
 
   public double getRelativeRPM(double dist){
     if (dist > 12){
@@ -139,46 +146,34 @@ public class ShooterSubsystem extends SubsystemBase {
   public void shootFlywheels(double dst){
     runShooter(getRelativeRPM(dst));
   }
-  public class ShootingPhysics {
-    //all in metric system 
-    private double distance;
-    private double maxHeight; 
-    private int shooterAngle = 24;
-    private double shooterHeight = 0.3048;
-    private final double gravity = 9.8;
-    private int flywheelRadius = 4;
-    private double targetHeight = 2.7432;
-
-    public ShootingPhysics(){}
 
 
-    public double RPMtoAngularVelocity(int rpm){
-        double omega = (2 * Math.PI * rpm) / 60;
-        return omega;
-    }
-    public double angularToLinearVelocity(double angularVelocity){
-        //two flywheels 
-        double velocity = RPMtoAngularVelocity(4000) * flywheelRadius; 
-        return velocity; 
-    }
+  public double RPMtoAngularVelocity(int rpm){
+      double omega = (2 * Math.PI * rpm) / 60;
+      return omega;
+  }
+  public double angularToLinearVelocity(double angularVelocity){
+      //two flywheels 
+      double velocity = RPMtoAngularVelocity(4000) * flywheelRadius; 
+      return velocity; 
+  }
 
-    public double linearToRPM(double v){
-        double angular = v / flywheelRadius;
-        double rpm = (angular * 60) / (2 * Math.PI);
-        return rpm;
-    }
+  public double linearToRPM(double v){
+      double angular = v / flywheelRadius;
+      double rpm = (angular * 60) / (2 * Math.PI);
+      return rpm;
+  }
 
-    public double ballMaxHeight(double dist, double initalVelocity, int launchAngle, double launchHeight, double gravity){
-        double maxHeight = ((dist * Math.tan(launchAngle)) + (0.5 * gravity * Math.pow(Math.cos(launchAngle),2)) + launchHeight);
-        return maxHeight;
-    }
+  public double ballMaxHeight(double dist, double initalVelocity, int launchAngle, double launchHeight){
+      double maxHeight = ((dist * Math.tan(launchAngle)) + (0.5 * gravity * Math.pow(Math.cos(launchAngle),2)) + launchHeight);
+      return maxHeight;
+  }
 
-    public double RPMforDistanceX(double dist, double launchAngle){
-        //2 flywheels, rpm / 2 
-        //dist is limelighttargeting.getdistance()
-        double neededVelocity = Math.sqrt((dist * 9.8) / (Math.sin(2 * launchAngle)));
-        double RPM = linearToRPM(neededVelocity);
-        return RPM;
-    }
-}
+  public double RPMforDistanceX(double dist, double launchAngle){
+      //2 flywheels, rpm / 2 
+      //dist is limelighttargeting.getdistance()
+      double neededVelocity = Math.sqrt((dist * 9.8) / (Math.sin(2 * launchAngle)));
+      double RPM = linearToRPM(neededVelocity);
+      return RPM;
+  }    
 }
