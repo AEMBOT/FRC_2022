6106561359410ad259d980ffcb81e4557d87ff8d
Patrick Higgins <phiggins@cs.uoregon.edu>
2022-02-19 11:43:38 -0800

    intake homing

---
+++ src/main/java/frc/robot/commands/HomeIntakeCommand.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** Homes the intake to its "max" point. */
+public class HomeIntakeCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final IntakeSubsystem m_subsystem;
+
+  private final double kLiftRangeOfMotion = 50;
+
+  /**
+   * Creates a new HomeIntakeCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public HomeIntakeCommand(IntakeSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_subsystem.setLiftPower(.1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    if (!interrupted) {
+      double max = m_subsystem.getLiftPosition();
+      double min = max - kLiftRangeOfMotion;
+      m_subsystem.setHome(min, max);
+    }
+
+    m_subsystem.setLiftPower(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    boolean isStopped = m_subsystem.isStopped();
+    return isStopped;
+  }
+}
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,33 +6,70 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.hardware.ClosedLoopSparkMax;
 
 public class IntakeSubsystem extends SubsystemBase{
-    //TODO: move these later 
-    private final int kliftLeftPort = 7;
-    private final int kliftRightPort = 8;
+    // TODO: move these later 
+    private final int kLiftLeftPort = 7;
+    private final int kLiftRightPort = 8;
     private final int kRollerPort = 9;
 
-    public IntakeSubsystem(){
-        roller.kI(000001);
-        liftLeft.follow(liftRight,false);
-    }
-
-    private final CANSparkMax liftLeft = new CANSparkMax(kliftLeftPort, MotorType.kBrushless);
-    private final CANSparkMax liftRight = new CANSparkMax(kliftRightPort, MotorType.kBrushless);
+    // motor controllers
+    private final CANSparkMax liftLeft = new CANSparkMax(kLiftLeftPort, MotorType.kBrushless);
+    private final CANSparkMax liftRight = new CANSparkMax(kLiftRightPort, MotorType.kBrushless);
+    
     private final ClosedLoopSparkMax roller = new ClosedLoopSparkMax(kRollerPort, MotorType.kBrushless);
 
-    public void setRPM(double rpm){
-        roller.setVelocity(rpm);
+    // whether the subsystem is successfully homed to its max point
+    private boolean homingComplete = false;
+
+    // The encoder positions for the lowest and highest allowed
+    private double m_lowestAllowedPosition;
+    private double m_highestAllowedPosition;
+
+    // motor geared 125:1 -> 24:72 gearing
+    public IntakeSubsystem() {
+
+        // TODO: set position conversion factor
+        double factor = 1;
+
+        // I value needs to be nonzero in order for closed loop PID to work
+        roller.kI(000001);
+
+        // set the left side to follow the right side, invert=false
+        liftLeft.follow(liftRight, false);
+
+        // set the position conversion factors for the lift encoders
+        liftRight.getEncoder().setPositionConversionFactor(factor);
     }
 
-    public void setAngle(double angle){
-
+    /** set the intake mechanism to run at the target RPM */
+    public void setRPM(double rpm) {
+        final double kGearRatio = 1/12;
+        roller.setVelocity(rpm * kGearRatio);
     }
 
-    public void calibrate(){
-        
+    /** sets the range of motion for the intake lift. */
+    public void setHome(double min, double max) {
+
+        if (homingComplete) {
+            System.out.println("Intake being re-homed!");
+        }
+
+        m_lowestAllowedPosition = min;
+        m_highestAllowedPosition = max;
+
+        homingComplete = true;
     }
 
-    public void setPower(double power){
+    public boolean isStopped() {
+        final double minVelocityTolerance = 0;
+        return (Math.abs(liftLeft.getEncoder().getVelocity()) <= 0) || (Math.abs(liftRight.getEncoder().getVelocity()) <= 0);
+    }
+
+    public void setLiftPower(double power) {
         liftRight.set(power);
+    }
+
+    /** gets the encoder position of the left lift */
+    public double getLiftPosition() {
+        return liftRight.getEncoder().getPosition();
     }
 }
