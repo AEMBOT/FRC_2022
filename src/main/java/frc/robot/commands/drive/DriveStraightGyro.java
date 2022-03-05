f1d4f84bd7554fc23d4d3fe5db7c9687bd69e56d
Zane Othman-Gomez <zane.othman@gmail.com>
2022-03-05 15:06:52 -0800

    DriveStraightGyro is now in a semi-working state
    It does drive far further than it should, but I'll work on tuning it
    more in the future.

--- src/main/java/frc/robot/commands/drive/DriveStraightGyro.java
+++ src/main/java/frc/robot/commands/drive/DriveStraightGyro.java
@@ -1,32 +1,32 @@
 package frc.robot.commands.drive;
 
-import static frc.robot.Constants.DriveConstants.*;
 import static frc.robot.Constants.DriveConstants.StraightPID.*;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveStraightGyro extends CommandBase {
-  private final Timer m_timer = new Timer();
   private final PIDController m_gyroPID = new PIDController(0.01, 0, 0);
+  private final SimpleMotorFeedforward m_feedforward =
+      new SimpleMotorFeedforward(kSVolts, kVVoltMetersPerSecond);
+  private final ProfiledPIDController m_profile =
+      new ProfiledPIDController(0.1, kI, kD, new TrapezoidProfile.Constraints(1.5, 0.5));
 
-  private TrapezoidProfile m_profile;
   private double m_distance;
   private DriveSubsystem m_drive;
 
-  public DriveStraightGyro(double distance, double offsetAngle, DriveSubsystem drive) {
-    // Create a trapezoidal motion profile
-    m_profile =
-        new TrapezoidProfile(
-            // TODO: Tune these constraints, since they were arbitrary
-            new TrapezoidProfile.Constraints(1.5, 1.0), new TrapezoidProfile.State(distance, 0));
-
+  public DriveStraightGyro(double distance, DriveSubsystem drive) {
     // Set the gyro PID setpoint to the desired offset angle
-    m_gyroPID.setSetpoint(offsetAngle);
+    m_gyroPID.setSetpoint(0);
+    m_gyroPID.enableContinuousInput(-180, 180);
+
+    // Set setpoint of the drive PID controller
+    m_profile.setGoal(new TrapezoidProfile.State(distance, 0));
 
     m_distance = distance;
     m_drive = drive;
@@ -43,33 +43,25 @@ public class DriveStraightGyro extends CommandBase {
     // Reset the drive motor encoders
     m_drive.resetEncoders();
 
-    // Start the timer for profiling purposes
-    m_timer.start();
+    // Reset the gyro
+    m_drive.resetHeading();
+
+    m_profile.reset(m_drive.getLeftEncoderPosition());
   }
 
   @Override
   public void execute() {
-    // Calculate the setpoint velocity from the motion profile
-    double profileVelocity = m_profile.calculate(m_timer.get()).velocity / kRPMToMetersPerSecond;
-    SmartDashboard.putNumber("DriveStraightGyro Velocity", profileVelocity);
-
     // Use a PID controller to adjust the velocities to ensure the robot drives at the right angle
+    double forwardSpeed =
+        m_feedforward.calculate(m_profile.getSetpoint().velocity)
+            + m_profile.calculate(m_drive.getLeftEncoderPosition(), m_distance);
+    SmartDashboard.putNumber("Gyro Straight Speed", forwardSpeed);
     double angleAdjustment = m_gyroPID.calculate(m_drive.getHeading());
-    m_drive.driveAtVelocity(profileVelocity - angleAdjustment, profileVelocity + angleAdjustment);
-  }
-
-  @Override
-  public void end(boolean _interrupted) {
-    // Stop the motors
-    m_drive.driveAtVelocity(0, 0);
-
-    // Stop and reset the timer
-    m_timer.stop();
-    m_timer.reset();
+    m_drive.arcadeDrive(forwardSpeed, angleAdjustment, false);
   }
 
   @Override
   public boolean isFinished() {
-    return m_drive.smartMotionAtGoal(m_distance);
+    return m_profile.atGoal();
   }
 }
