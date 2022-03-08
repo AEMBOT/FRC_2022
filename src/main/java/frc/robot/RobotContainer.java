a109c52f7084c0cb0d023a04112e1ff550742b21
Nolan Hergert <nolan.hergert@gmail.com>
2022-03-07 20:53:05 -0800

    added while held to button to shoot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -89,7 +89,7 @@ public class RobotContainer {
             new InstantCommand(() -> m_shooterSubsystem.toggleShooter(), m_shooterSubsystem));*/
     
     new JoystickButton(m_driverController, Button.kX.value)
-    .whenPressed(
+    .whileHeld(
         new TeleOpShooter(m_shooterSubsystem));
 
     new JoystickButton(m_driverController, Button.kY.value)
