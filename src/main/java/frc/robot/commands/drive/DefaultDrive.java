a39a72f94a0d8ec1e2431976fc0db01db86155e0
Nolan Hergert <nolan.hergert@gmail.com>
2022-03-08 21:04:01 -0800

    Add deadzone and exponential ramping

--- src/main/java/frc/robot/commands/drive/DefaultDrive.java
+++ src/main/java/frc/robot/commands/drive/DefaultDrive.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 import java.util.function.DoubleSupplier;
+import edu.wpi.first.math.MathUtil;
 
 public class DefaultDrive extends CommandBase {
   private final DriveSubsystem m_drive;
@@ -12,8 +13,18 @@ public class DefaultDrive extends CommandBase {
   private final DoubleSupplier m_left;
   private final DoubleSupplier m_right;
 
+  //ramping constants
+  private final double rampNew = 0.9;
+  private final double rampOld = 1 - rampNew;
+
   // Drive at full speed for driver practice
   private double speedMultiplier = 1.0;
+
+  //Deadzone, choose number from range (0,1)
+  private double deadzone = 0.1;
+
+  //Steering denominator
+  private double steeringDenominator = 2;
 
   public DefaultDrive(DriveSubsystem drive, DoubleSupplier left, DoubleSupplier right) {
     m_drive = drive;
@@ -22,14 +33,36 @@ public class DefaultDrive extends CommandBase {
     addRequirements(drive);
   }
 
+  double forwardPowerPrev = 0;
+  double rotationPowerPrev = 0;
+
   @Override
   public void execute() {
     // Log the powers to the dashboard
-    double forwardPower = speedMultiplier * m_left.getAsDouble();
+    double forwardPower = speedMultiplier * (MathUtil.applyDeadband(m_left.getAsDouble(), deadzone));
+    forwardPower = forwardPower * rampNew + forwardPowerPrev * rampOld;
+    forwardPowerPrev = forwardPower;
     SmartDashboard.putNumber("power", forwardPower);
-    double rotationPower = speedMultiplier * -m_right.getAsDouble();
+    
+    double rotationPower = speedMultiplier * ((MathUtil.applyDeadband(m_right.getAsDouble(), deadzone)) / steeringDenominator);
+    rotationPower = rotationPower * rampNew + rotationPowerPrev + rampOld;
     SmartDashboard.putNumber("rotation", rotationPower);
 
-    m_drive.arcadeDrive(forwardPower, rotationPower, false);
+    m_drive.arcadeDrive(forwardPower, rotationPower, true);
   }
+
+  /*
+    linear teleop profiling
+    if accellerating
+      while not at goal speed
+        current speed += 0.2
+        pass speed to drive control
+    else (deccelerating)
+      while not at goal speed
+        current speed -= 0.2
+        pass speed to drive control
+
+    Notes: by using a slop of .02, the robot should thearetically take 1 seccond to reach full speed from 0
+  */
+
 }
