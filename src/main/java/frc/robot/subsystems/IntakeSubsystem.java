cb397fcdc0fbb251e4e7ef5bdb83aac6145f52ab
Troy Lopez <nicktheninja4me@gmail.com>
2022-03-08 21:01:50 -0800

    hub and intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -48,6 +48,9 @@ public class IntakeSubsystem extends SubsystemBase {
     // upper/inner roller follows the outside
     indexEntryRoller.follow(intakeRoller);
 
+    liftLeft.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    liftRight.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    
     // set the left side to follow the right side, invert=false
     liftLeft.follow(liftRight, true);
 
