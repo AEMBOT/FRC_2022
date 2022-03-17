fc2b455dd68bc1b72ce0168711f4b9a60fff098b
Zane Othman-Gomez <zane.othman@gmail.com>
2022-03-17 11:51:14 -0700

    Remove Turn{Off,On}LimelightLEDs commands

--- src/main/java/frc/robot/commands/drive/AlignWithHubSmart.java
+++ src/main/java/frc/robot/commands/drive/AlignWithHubSmart.java
@@ -2,17 +2,26 @@ package frc.robot.commands.drive;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.utilities.TurnOnLimelightLEDs;
 import frc.robot.hardware.Limelight;
+import frc.robot.hardware.Limelight.LEDMode;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class AlignWithHubSmart extends SequentialCommandGroup {
+  private Limelight m_limelight;
+
   public AlignWithHubSmart(Limelight limelight, DriveSubsystem drive) {
     addCommands(
-        new TurnOnLimelightLEDs(limelight),
+        // Allow some time for the Limelight LEDs to actually turn on
         new WaitCommand(0.2),
+
+        // Turn towards the hub
         new TurnToAngleSmart(() -> -limelight.getX(), drive).withTimeout(0.5)
-        // new TurnOffLimelight(limelight)
         );
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    m_limelight.setLEDMode(LEDMode.On);
   }
 }
